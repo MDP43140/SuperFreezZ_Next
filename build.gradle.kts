b104f4fff6997bdeafa418e8cce13ac0480632ed
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2025-02-27 07:38:16 +0700

    Update dependencies
    + Gradle wrapper: 8.12 > 8.13
    + com.android.*: 8.7.3 > 8.8.1
    + org.jetbrains.kotlin.android: 2.1.0 > 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
 //auto suffixed with .gradle.plugin
 	id("androidx.baselineprofile") version "1.3.3" apply false // optional
-	id("com.android.application") version "8.7.3" apply false
-	id("com.android.test") version "8.7.3" apply false // optional, baselineprofile dependency
-	kotlin("android") version "2.1.0" apply false // kotlin("android") == "org.jetbrains.kotlin.android"
+	id("com.android.application") version "8.8.1" apply false
+	id("com.android.test") version "8.8.1" apply false // optional, baselineprofile dependency
+	kotlin("android") version "2.1.10" apply false // kotlin("android") == "org.jetbrains.kotlin.android"
 }
 tasks.withType(JavaCompile::class.java){
 	options.compilerArgs.add("-Xlint:all")
