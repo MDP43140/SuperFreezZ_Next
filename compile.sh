6a0d99deba6d452c9062028facfbd5433de9bc17
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2025-06-11 19:22:47 +0700

    compile.sh fixes
    + Make sure to use correct Java version (gradle compiler
    	really hates incorrect version down to single digit)
    + Fix files that cant be removed by Gradle due to "its currently used"
    	causing issues where it makes app size 2-3x bigger and in some cases
    	old codes overriden newly compiled codes (more prevalent on
    	NTFS partitions, which for some reason is where i stored
    	all of my projects in)

--- compile.sh
+++ compile.sh
@@ -2,4 +2,29 @@
 clear
 ops="$1"
 [ "$ops" ] || ops="assembleRelease"
+
+# Make sure to use correct Java version (compiler
+# really hates incorrect version down to single digit)
+for i in \
+	/usr/lib/jvm/java-21-openjdk-amd64 \
+	/usr/lib/jvm/temurin-24-jdk-amd64;do
+	if [ -d "$i" ];then
+		JAVA_HOME="$i"
+		break
+	fi
+done
+
+# fix files not removed because "is used" causing issues
+# where it makes app size 2x bigger and old codes overriden
+# newly compiled codes (more prevalent on NTFS partitions)
+rm "./*/build/intermediates/dex/release/mergeDexRelease/*"
+rm "./*/build/intermediates/dex/release/.fuse_hidden*.tmp"
+for subproj in ./*/build;do
+	for file in $(ls ${subproj}/intermediates/dex/release/mergeDexRelease);do
+		file=$(echo "$file" | sed "s/\*$//")
+		mv "${subproj}/intermediates/dex/release/mergeDexRelease/${file}" \
+			 "${subproj}/intermediates/dex/release/${file}.tmp";
+	done
+done
+
 exec sh gradlew $ops
